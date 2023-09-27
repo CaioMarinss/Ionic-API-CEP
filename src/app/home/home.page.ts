d8792ba348ed1b0b54a58285c50e081414e2eac5
Caio Marins <marinsroyale@gmail.com>
2023-09-26 21:27:33 -0300

    Conclusão da consulta pelo CEP

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage{
 
-  constructor() {}
+  cepUser: any;
+  cep: any;
 
+
+  constructor(private http: HttpClient) {}
+
+  Consultar() {
+    this.cep = this.http.get(`https://viacep.com.br/ws/${this.cepUser}/json/`).subscribe(
+      (data) =>{
+        this.cep = data
+      }
+    )
+  }
 }
